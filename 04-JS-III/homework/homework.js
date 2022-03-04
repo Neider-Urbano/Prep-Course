15eb6f399ba17ddc6d1bcb240380df76299ef2e5
Neider Urbano <jjuly8julianur@gmail.com>
2022-03-04 16:18:04 -0500

    modificacion codigo homework 3 de Henry

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -36,7 +36,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array[array.length] = elemento;
+  array.push(elemento);
   return array;
 }
 
